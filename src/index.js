df55f550f9febbc2b64680146ddd9e06de3b531d
ericblam <marcusgladir@gmail.com>
2021-06-05 15:52:51 -0400

    Add config schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 // Imports
 const express = require('express')
 const fs = require('fs')
+const Validator = require('jsonschema').Validator;
 
 // Config
+const configSchema = JSON.parse(fs.readFileSync('src/config.schema.json'));
 const config = JSON.parse(fs.readFileSync('src/config.json'));
-const port = config.port;
+const configValidator = new Validator();
+
+if (!configValidator.validate(config, configSchema).valid) {
+    console.log('Config is malformed');
+    process.exit();
+}
+
+const port = config.server.port ?? 8080;
 
 const app = express();
 
